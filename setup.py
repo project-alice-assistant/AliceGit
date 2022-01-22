94f622a4c09a194d711271e854b8cef378b69d57
Psychokiller1888 <laurentchervet@bluewin.ch>
2022-01-22 15:15:42 +0100

    :art: Production stable

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
 		'coveralls~=3.3.1'
 	],
 	classifiers=[
-		"Development Status :: 3 - Alpha",
+		"Development Status :: 5 - Production/Stable",
 		"Environment :: Console",
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
